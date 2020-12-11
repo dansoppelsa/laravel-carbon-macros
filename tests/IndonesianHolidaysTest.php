dd109923edcea8d022f9ff46b4220920d3ba21e0
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2020-12-11 15:20:36 +0700

    test: Indonesia labor day

--- tests/IndonesianHolidaysTest.php
+++ tests/IndonesianHolidaysTest.php
@@ -55,4 +55,29 @@ class IndonesianHolidaysTest extends TestCase
             '2020-06-01' => ['2020-06-01', true],
         ];
     }
+
+    /**
+     * @dataProvider provideIndonesiaLaborDayData
+     */
+    public function test_it_recognizes_indonesia_labor_day($date, $validity)
+    {
+        $date = Carbon::parse($date);
+
+        $this->assertSame($validity, $date->isIndonesiaLaborDay());
+    }
+
+    public function provideIndonesiaLaborDayData()
+    {
+        return [
+            '1945-02-20' => ['1945-02-20', true],
+            '1955-06-02' => ['1955-06-02', false],
+            '1965-07-01' => ['1965-07-01', false],
+            '1975-02-20' => ['1975-02-20', true],
+            '1985-12-01' => ['1985-12-01', false],
+            '1995-02-20' => ['1995-02-20', true],
+            '2005-05-05' => ['2005-05-05', false],
+            '2015-02-20' => ['2015-02-20', true],
+            '2020-02-20' => ['2020-02-20', true],
+        ];
+    }
 }
