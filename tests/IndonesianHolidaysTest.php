ea5cce98f3e614332851653eb5e11cd46afae384
Panji Setya Nur Prawira <kstar.panjinamjaelf@gmail.com>
2020-12-11 15:26:03 +0700

    test: Kartini day

--- tests/IndonesianHolidaysTest.php
+++ tests/IndonesianHolidaysTest.php
@@ -80,4 +80,25 @@ class IndonesianHolidaysTest extends TestCase
             '2020-02-20' => ['2020-02-20', true],
         ];
     }
+
+    /**
+     * @dataProvider provideKartiniDayData
+     */
+    public function test_it_recognizes_kartini_day($date, $validity)
+    {
+        $date = Carbon::parse($date);
+
+        $this->assertSame($validity, $date->isKartiniDay());
+    }
+
+    public function provideKartiniDayData()
+    {
+        return [
+            '1990-04-21' => ['1990-04-21', true],
+            '1995-06-02' => ['1995-06-02', false],
+            '2000-04-21' => ['2000-04-21', true],
+            '2015-02-20' => ['2015-02-20', false],
+            '2020-04-21' => ['2020-04-21', true],
+        ];
+    }
 }
