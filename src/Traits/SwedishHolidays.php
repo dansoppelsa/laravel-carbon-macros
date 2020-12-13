c71638d5f62d906cdbc21dfd86cdaab9dfcd0618
Dan Soppelsa <dsoppelsa@gmail.com>
2020-12-12 21:49:36 -0500

    Remove use of Carbon month helpers for backward compatibility

--- src/Traits/SwedishHolidays.php
+++ src/Traits/SwedishHolidays.php
@@ -10,22 +10,19 @@ trait SwedishHolidays
     {
         Carbon::macro('isSwedishMidsummerDay', function () {
             // Midsummer Day, time to eat herring, potatoes and sour cream.
-
-            return $this->month === Carbon::JUNE &&
-                $this->weekday() === Carbon::SATURDAY &&
-                ($this->day >= 20 && $this->day <= 26);
+            return  $this->month === 6 &&
+                    $this->weekday() === Carbon::SATURDAY &&
+                    ($this->day >= 20 && $this->day <= 26);
         });
 
         Carbon::macro('isChristmasEve', function () {
             // Swedes celebrate Christmas Eve, Christmas Day is just another holiday.
-
-            return $this->month === Carbon::DECEMBER && $this->day === 24;
+            return $this->month === 12 && $this->day === 24;
         });
 
         Carbon::macro('isSwedishNationalDay', function () {
             // Previously knows as Swedish Flag Day before 1983.
-
-            return $this->month === Carbon::JUNE && $this->day === 6;
+            return $this->month === 6 && $this->day === 6;
         });
     }
 }
