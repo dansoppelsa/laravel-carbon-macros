5ad582528ef87abfb31f9bd0f6c5cc9b4c362304
Dan Soppelsa <dsoppelsa@gmail.com>
2020-12-14 23:07:42 -0500

    Add links for German holidays

--- src/Traits/GermanHolidays.php
+++ src/Traits/GermanHolidays.php
@@ -12,6 +12,7 @@ trait GermanHolidays
             /*
              * The german labour day differs from the Canadian one.
              * In Germany, labour day is celebrated on May 1st.
+             * https://en.wikipedia.org/wiki/Public_holidays_in_Germany
              */
             return $this->month === 5 && $this->day === 1;
         });
@@ -22,7 +23,8 @@ trait GermanHolidays
              * of Jesus Christ as the Son of God to His Father in heaven.
              * Ascension Day is celebrated on the 40th day of the Easter
              * season, 39 days after Easter Sunday. Therefore, the feast
-             * always falls on a Thursday. 
+             * always falls on a Thursday.
+             * https://en.wikipedia.org/wiki/Public_holidays_in_Germany
              */
             return $this->clone()
                 ->subDays(39)
@@ -32,7 +34,8 @@ trait GermanHolidays
         Carbon::macro('isWhitSunday', function () {
             /*
              * The Christian holiday of Pentecost or Whitsunday is celebrated
-             * the 50th day (the seventh Sunday) from Easter Sunday. 
+             * the 50th day (the seventh Sunday) from Easter Sunday.
+             * https://en.wikipedia.org/wiki/Public_holidays_in_Germany
              */
             return $this->clone()
                 ->subDays(49)
@@ -54,6 +57,7 @@ trait GermanHolidays
         Carbon::macro('isWhitMonday', function () {
             /*
              * Whit Monday is the holiday celebrated the day after Pentecost.
+             * https://en.wikipedia.org/wiki/Public_holidays_in_Germany
              */
             return $this->clone()
                 ->subDays(1)
@@ -68,8 +72,8 @@ trait GermanHolidays
             /*
              * In Germany, Corpus Christi is a public holiday in some states.
              * It takes place 60 days after Easter Sunday.
+             * https://en.wikipedia.org/wiki/Public_holidays_in_Germany
              */
-
             return $this->clone()
                 ->subDays(60)
                 ->isEasterSunday();
@@ -85,8 +89,8 @@ trait GermanHolidays
              * there existed a single German state. German Unity Day on 3 October
              * has been the German National Holiday since 1990, when the
              * reunification was formally completed.
+             * https://en.wikipedia.org/wiki/Public_holidays_in_Germany
              */
-
             if ($this->year < 1990) {
                 return false;
             }
@@ -99,11 +103,7 @@ trait GermanHolidays
              * Germans celebrate Christmas Eve (German: Heiliger Abend, Heiligabend),
              * Christmas Day is just another holiday.
              */
-            if (\method_exists($this, 'isChristmasEve')) {
-                return $this->isChristmasEve();
-            }
-
-            return $this->month === 12 && $this->day === 24;
+            return $this->isChristmasEve();
         });
 
         Carbon::macro('isHeiligAbend', function () {
